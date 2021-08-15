711b2d38aafa0dbf83b2090261c35cf5ad9a5074
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2021-08-16 00:48:42 +0200

    fdsfdfd

--- scripts/discord/release-new-extensions.js
+++ scripts/discord/release-new-extensions.js
@@ -1,5 +1,11 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const {Intents} = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
 
 client.login(process.env.DISCORD_TOKEN);
 
