f62febdf9512c6f5faf8e026828b17abc32e1fcb
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2021-09-26 00:19:43 +0200

    remove extension tweet

--- scripts/twitter/publish-to-twitter.js
+++ scripts/twitter/publish-to-twitter.js
@@ -6,6 +6,7 @@ const compareVersions = require("compare-versions");
 
 const allExtensions = extensionConfigs();
 const cachedNameToVersion = new Map(twitterCache.map(ext => [ext.title, ext.version]));
+const cachedNameToOutdated = new Map(twitterCache.map(ext => [ext.title, ext.isOutdated ?? false]));
 
 const writeCache = () => {
     const dir = __dirname + '/../../.auto-generated/twitter';
@@ -18,8 +19,10 @@ const writeCache = () => {
 const publishExtensionTweet = async (client, ext) => {
     const readMore = ext.readme !== undefined ? ` \n\nRead more: ${ext.readme}` : ""
     const author = ext.authors[0];
+    const authorInTweet = author.twitter !== undefined ? "@" + author.twitter : author.name;
+
     const tweetContents = `A new extension "${ext.title}" was published to the G-ExtensionStore!` +
-        ` Created by ${author.twitter !== undefined ? "@" + author.twitter : author.name}.${readMore}`;
+        ` Created by ${authorInTweet}.${readMore}`;
     // const mediaId = await client.v1.uploadMedia('image.png');
 
     const screenshotPath = `./store/extensions/${ext.title}/screenshot.png`;
@@ -36,14 +39,26 @@ const publishExtensionTweet = async (client, ext) => {
 const updateExtensionTweet = async (client, oldVersion, ext) => {
     const readMore = ext.readme !== undefined ? ` \n\nRead more: ${ext.readme}` : ""
     const author = ext.authors[0];
+    const authorInTweet = author.twitter !== undefined ? "@" + author.twitter : author.name;
+
     const tweetContents = `"${ext.title}" was updated from ${oldVersion} to ${ext.version}!` +
-        ` Created by ${author.twitter !== undefined ? "@" + author.twitter : author.name}.${readMore}`;
+        ` Created by ${authorInTweet}.${readMore}`;
 
     console.log(`Updating ${ext.title} on twitter...`)
     return await client.v1.tweet(tweetContents);
 }
 
-const bumpTwitter = async (newExtensions, updatedExtensions) => {
+const removeExtensionTweet = async (client, ext) => {
+    const author = ext.authors[0];
+    const authorInTweet = author.twitter !== undefined ? "@" + author.twitter : author.name;
+
+    const tweetContents = `"${ext.title}" by ${authorInTweet} was removed from the G-ExtensionStore`;
+
+    console.log(`Removing ${ext.title} on twitter...`)
+    return await client.v1.tweet(tweetContents);
+}
+
+const bumpTwitter = async (newExtensions, updatedExtensions, removedExtensions) => {
     const client = new TwitterApi({
         appKey: process.env.TWITTER_APPKEY,
         appSecret: process.env.TWITTER_APPSECRET,
@@ -56,6 +71,7 @@ const bumpTwitter = async (newExtensions, updatedExtensions) => {
         twitterCache.push({
             title: ext.title,
             version: ext.version,
+            isOutdated: false,
             _object: ext,
             tweets: [{
                 id_str: actualTweet.id_str,
@@ -78,6 +94,7 @@ const bumpTwitter = async (newExtensions, updatedExtensions) => {
 
         const cacheObj = twitterCache.find(obj => obj.title === ext.title);
         cacheObj.version = ext.version;
+        cacheObj.isOutdated = false;
         cacheObj._object = ext;
         cacheObj.tweets.push({
             id_str: actualTweet.id_str,
@@ -92,13 +109,36 @@ const bumpTwitter = async (newExtensions, updatedExtensions) => {
     }
 
     writeCache();
+
+    for (const ext of removedExtensions) {
+        const actualTweet = await removeExtensionTweet(client, ext);
+
+        const cacheObj = twitterCache.find(obj => obj.title === ext.title);
+        cacheObj.version = ext.version;
+        cacheObj.isOutdated = true;
+        cacheObj._object = ext;
+        cacheObj.tweets.push({
+            id_str: actualTweet.id_str,
+            extVersion: ext.version,
+            type: "remove",
+            timestamp: Date.now(),
+            locked: false,      // not implemented
+            score: 0            // not implemented
+        });
+
+        await new Promise(r => setTimeout(r, 5000));
+    }
+
+    writeCache();
 }
 
 
-const newExtensions = allExtensions.filter((ext) => !cachedNameToVersion.has(ext.title));
-const updatedExtensions = allExtensions.filter((ext) =>
+const newExtensions = allExtensions.filter((ext) => !ext.isOutdated && !cachedNameToVersion.has(ext.title));
+const updatedExtensions = allExtensions.filter((ext) => !ext.isOutdated &&
     cachedNameToVersion.has(ext.title) && compareVersions(ext.version, cachedNameToVersion.get(ext.title)) > 0);
+const removedExtensions = allExtensions.filter((ext) => ext.isOutdated &&
+    cachedNameToOutdated.has(ext.title) && !cachedNameToOutdated.get(ext.title));
 
-if (newExtensions.length > 0 || updatedExtensions.length > 0) {
-    bumpTwitter(newExtensions, updatedExtensions).then(r => console.log("finished"));
+if (newExtensions.length > 0 || updatedExtensions.length > 0 || removedExtensions.length > 0) {
+    bumpTwitter(newExtensions, updatedExtensions, removedExtensions).then(r => console.log("finished"));
 }
